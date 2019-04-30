db67c3496abd74e596fdd2bf34a00e93de2ac0c5
Chigusa <chigusaweb66@gmail.com>
2019-04-30 14:40:11 +0900

    初回PUSH

---
+++ OpenCVSharpSample/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace OpenCVSharpSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_sample1 = new System.Windows.Forms.Button();
+            this.b_grayscale = new System.Windows.Forms.Button();
+            this.b_template1 = new System.Windows.Forms.Button();
+            this.b_template2 = new System.Windows.Forms.Button();
+            this.b_face = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b_sample1
+            // 
+            this.b_sample1.Location = new System.Drawing.Point(44, 23);
+            this.b_sample1.Name = "b_sample1";
+            this.b_sample1.Size = new System.Drawing.Size(207, 82);
+            this.b_sample1.TabIndex = 0;
+            this.b_sample1.Text = "基本";
+            this.b_sample1.UseVisualStyleBackColor = true;
+            this.b_sample1.Click += new System.EventHandler(this.B_sample1_Click);
+            // 
+            // b_grayscale
+            // 
+            this.b_grayscale.Location = new System.Drawing.Point(44, 123);
+            this.b_grayscale.Name = "b_grayscale";
+            this.b_grayscale.Size = new System.Drawing.Size(207, 82);
+            this.b_grayscale.TabIndex = 1;
+            this.b_grayscale.Text = "グレースケール";
+            this.b_grayscale.UseVisualStyleBackColor = true;
+            this.b_grayscale.Click += new System.EventHandler(this.B_grayscale_Click);
+            // 
+            // b_template1
+            // 
+            this.b_template1.Location = new System.Drawing.Point(44, 224);
+            this.b_template1.Name = "b_template1";
+            this.b_template1.Size = new System.Drawing.Size(207, 82);
+            this.b_template1.TabIndex = 2;
+            this.b_template1.Text = "テンプレートマッチ1";
+            this.b_template1.UseVisualStyleBackColor = true;
+            this.b_template1.Click += new System.EventHandler(this.B_template1_Click);
+            // 
+            // b_template2
+            // 
+            this.b_template2.Location = new System.Drawing.Point(44, 331);
+            this.b_template2.Name = "b_template2";
+            this.b_template2.Size = new System.Drawing.Size(207, 82);
+            this.b_template2.TabIndex = 3;
+            this.b_template2.Text = "テンプレートマッチ2";
+            this.b_template2.UseVisualStyleBackColor = true;
+            this.b_template2.Click += new System.EventHandler(this.B_template2_Click);
+            // 
+            // b_face
+            // 
+            this.b_face.Location = new System.Drawing.Point(286, 23);
+            this.b_face.Name = "b_face";
+            this.b_face.Size = new System.Drawing.Size(207, 82);
+            this.b_face.TabIndex = 4;
+            this.b_face.Text = "顔認識";
+            this.b_face.UseVisualStyleBackColor = true;
+            this.b_face.Click += new System.EventHandler(this.B_face_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.b_face);
+            this.Controls.Add(this.b_template2);
+            this.Controls.Add(this.b_template1);
+            this.Controls.Add(this.b_grayscale);
+            this.Controls.Add(this.b_sample1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b_sample1;
+        private System.Windows.Forms.Button b_grayscale;
+        private System.Windows.Forms.Button b_template1;
+        private System.Windows.Forms.Button b_template2;
+        private System.Windows.Forms.Button b_face;
+    }
+}
+
---
+++ OpenCVSharpSample/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace OpenCVSharpSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void B_sample1_Click(object sender, EventArgs e)
+        {
+            // 画像の読み込み
+            using (Mat mat = new Mat(@"D:\cs_source\img\neko.jpg"))
+            {
+                // 画像をウィンドウに表示
+                Cv2.ImShow("sample_show", mat);
+            }
+        }
+
+        private void B_grayscale_Click(object sender, EventArgs e)
+        {
+            // 画像の読み込み
+            using (Mat mat = new Mat(@"D:\cs_source\img\neko.jpg"))
+            using (Mat matGray = mat.CvtColor(ColorConversionCodes.BGR2GRAY))
+            {
+                // 画像をウィンドウに表示
+                Cv2.ImShow("grayscale_show", matGray);
+            }
+        }
+
+        private void B_template1_Click(object sender, EventArgs e)
+        {
+            // 検索対象の画像とテンプレート画像
+            using (Mat mat = new Mat(@"D:\cs_source\img\neko.jpg"))
+            using (Mat temp = new Mat(@"D:\cs_source\img\template1.jpg"))
+            using (Mat result = new Mat())
+            {
+
+                // テンプレートマッチ
+                Cv2.MatchTemplate(mat, temp, result, TemplateMatchModes.CCoeffNormed);
+
+                // 類似度が最大/最小となる画素の位置を調べる
+                OpenCvSharp.Point minloc, maxloc;
+                double minval, maxval;
+                Cv2.MinMaxLoc(result, out minval, out maxval, out minloc, out maxloc);
+
+                // しきい値で判断
+                var threshold = 0.9;
+                if (maxval >= threshold)
+                {
+
+                    // 最も見つかった場所に赤枠を表示
+                    Rect rect = new Rect(maxloc.X, maxloc.Y, temp.Width, temp.Height);
+                    Cv2.Rectangle(mat, rect, new OpenCvSharp.Scalar(0, 0, 255), 2);
+
+                    // ウィンドウに画像を表示
+                    Cv2.ImShow("template1_show", mat);
+
+                }
+                else
+                {
+                    // 見つからない
+                    MessageBox.Show("見つかりませんでした");
+                }
+
+            }
+
+        }
+
+        private void B_template2_Click(object sender, EventArgs e)
+        {
+
+            // 検索対象の画像とテンプレート画像
+            using (Mat mat = new Mat(@"D:\cs_source\img\neko2.jpg"))
+            using (Mat temp = new Mat(@"D:\cs_source\img\template1.jpg"))
+            using (Mat result = new Mat())
+            {
+
+                // テンプレートマッチ
+                Cv2.MatchTemplate(mat, temp, result, TemplateMatchModes.CCoeffNormed);
+
+                // しきい値の範囲に絞る
+                Cv2.Threshold(result, result, 0.8, 1.0, ThresholdTypes.Tozero);
+
+                while (true)
+                {
+                    // 類似度が最大/最小となる画素の位置を調べる
+                    OpenCvSharp.Point minloc, maxloc;
+                    double minval, maxval;
+                    Cv2.MinMaxLoc(result, out minval, out maxval, out minloc, out maxloc);
+
+                    var threshold = 0.8;
+                    if (maxval >= threshold)
+                    {
+
+                        // 見つかった場所に赤枠を表示
+                        Rect rect = new Rect(maxloc.X, maxloc.Y, temp.Width, temp.Height);
+                        Cv2.Rectangle(mat, rect, new OpenCvSharp.Scalar(0, 0, 255), 2);
+
+                        // 見つかった箇所は塗りつぶす
+                        Rect outRect;
+                        Cv2.FloodFill(result, maxloc, new OpenCvSharp.Scalar(0), out outRect, new OpenCvSharp.Scalar(0.1),
+                                    new OpenCvSharp.Scalar(1.0), FloodFillFlags.Link4);
+
+                    } else { 
+                        break;
+                    }
+                }
+
+                // ウィンドウに画像を表示
+                Cv2.ImShow("template2_show", mat);
+            }
+
+        }
+
+        private void B_face_Click(object sender, EventArgs e)
+        {
+
+            //顔の矩形を抽出
+            using (Mat mat = new Mat(@"D:\cs_source\img\face.jpg"))
+            {
+                // 分類機の用意
+                using (CascadeClassifier cascade = new CascadeClassifier(@"D:\cs_source\haarcascade_frontalface_default.xml"))
+                {
+                    foreach (Rect rectFace in cascade.DetectMultiScale(mat))
+                    {
+                        // 見つかった場所に赤枠を表示
+                        Rect rect = new Rect(rectFace.X, rectFace.Y, rectFace.Width, rectFace.Height);
+                        Cv2.Rectangle(mat, rect, new OpenCvSharp.Scalar(0, 0, 255), 2);
+                    }
+                }
+
+                // ウィンドウに画像を表示
+                Cv2.ImShow("face_show", mat);
+            }
+
+        }
+
+    }
+}
